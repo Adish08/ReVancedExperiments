53b77bc40c8817239e218e929cfbe7c32a241184
Adish08 <adish.sag@gmail.com>
2025-01-08 18:10:10 +0530

    Revert "Modify fingerprints for cherrygram"
    This reverts commit d216a5feaddee8daa623bc47c8b1bf626e3c9a96.

--- patches/src/main/kotlin/li/auna/patches/telegram/bypassintegrity/Fingerprints.kt
+++ patches/src/main/kotlin/li/auna/patches/telegram/bypassintegrity/Fingerprints.kt
@@ -1,33 +1,16 @@
 package li.auna.patches.telegram.bypassintegrity
 
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-import app.revanced.patcher.fingerprint.method.annotation.FingerprintAnnotation
-import app.revanced.patcher.fingerprint.method.annotation.StringsAnnotation
+import app.revanced.patcher.fingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
-import com.android.tools.smali.dexlib2.Opcode
 
-@FingerprintAnnotation(
-    strings = [
-        StringsAnnotation(["basicIntegrity", "ctsProfileMatch"])
-    ],
-    accessFlags = AccessFlags.PRIVATE or AccessFlags.SYNTHETIC
-)
-internal object IntegrityCheckFingerprint : MethodFingerprint(
-    returnType = "V",
-    parameters = listOf(),
-    customFingerprint = { methodDef, _ ->
-        methodDef.implementation?.instructions?.any { 
-            it.opcode == Opcode.CONST_STRING 
-        } == true
-    }
-)
+internal val bypassIntegrityFingerprint = fingerprint {
+    accessFlags(AccessFlags.PRIVATE, AccessFlags.SYNTHETIC)
+    returns("V")
+    strings("basicIntegrity", "ctsProfileMatch")
+}
 
-@FingerprintAnnotation
-internal object SignatureFingerprint : MethodFingerprint(
-    returnType = "Ljava/lang/String;",
-    strings = listOf(),
-    customFingerprint = { methodDef, classDef ->
-        methodDef.name == "getCertificateSHA256Fingerprint" 
-            && classDef.type.endsWith("Lorg/telegram/messenger/AndroidUtilities;")
+internal val spoofSignatureFingerprint = fingerprint {
+    custom { methodDef, classDef ->
+        methodDef.name == "getCertificateSHA256Fingerprint" && classDef.type.endsWith("Lorg/telegram/messenger/AndroidUtilities;")
     }
-)
+}
