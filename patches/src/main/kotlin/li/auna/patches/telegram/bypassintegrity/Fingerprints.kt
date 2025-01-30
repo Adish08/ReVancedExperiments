972a647f1117f6ab439d3c7772cd9a683ff265d0
Adish Sagarawat <69076256+Adish08@users.noreply.github.com>
2025-01-30 13:12:11 +0530

    Fix integrity

--- patches/src/main/kotlin/li/auna/patches/telegram/bypassintegrity/Fingerprints.kt
+++ patches/src/main/kotlin/li/auna/patches/telegram/bypassintegrity/Fingerprints.kt
@@ -4,12 +4,14 @@ import app.revanced.patcher.fingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 
 internal val bypassIntegrityFingerprint = fingerprint {
-    accessFlags = AccessFlags.PRIVATE or AccessFlags.SYNTHETIC
-    returnType = "V"
-    strings = listOf("basicIntegrity", "ctsProfileMatch", "MEQ")
-    customFingerprint = { methodDef, _ ->
-        methodDef.parameters.size == 1 && 
-        methodDef.parameterTypes.first() == "Lcom/google/android/gms/tasks/Task;"
+    parameters(
+        accessFlags = AccessFlags.PRIVATE.value or AccessFlags.SYNTHETIC.value,
+        returnType = "V",
+        strings = listOf("basicIntegrity", "ctsProfileMatch", "MEQ")
+    )
+    custom { methodDef, _ ->
+        methodDef.parameterTypes?.size == 1 &&
+        methodDef.parameterTypes?.first() == "Lcom/google/android/gms/tasks/Task;"
     }
 }
 
@@ -17,6 +19,6 @@ internal val spoofSignatureFingerprint = fingerprint {
     custom { methodDef, classDef ->
         methodDef.name == "getCertificateSHA256Fingerprint" && 
         (classDef.type.endsWith("Lorg/telegram/messenger/AndroidUtilities;") || 
-         classDef.type.endsWith("Luz/unnarsx/cherrygram/AndroidUtilities;")) // Added Cherrygram path
+         classDef.type.endsWith("Luz/unnarsx/cherrygram/AndroidUtilities;")) 
     }
 }
