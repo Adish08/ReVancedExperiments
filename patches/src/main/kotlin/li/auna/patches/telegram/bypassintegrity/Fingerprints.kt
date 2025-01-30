4df0c066adf7857ea9608bb16e398dd1322b0314
Adish Sagarawat <69076256+Adish08@users.noreply.github.com>
2025-01-30 12:53:09 +0530

    Add Cherrygram Path

--- patches/src/main/kotlin/li/auna/patches/telegram/bypassintegrity/Fingerprints.kt
+++ patches/src/main/kotlin/li/auna/patches/telegram/bypassintegrity/Fingerprints.kt
@@ -4,13 +4,19 @@ import app.revanced.patcher.fingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 
 internal val bypassIntegrityFingerprint = fingerprint {
-    accessFlags(AccessFlags.PRIVATE, AccessFlags.SYNTHETIC)
-    returns("V")
-    strings("basicIntegrity", "ctsProfileMatch")
+    accessFlags = AccessFlags.PRIVATE or AccessFlags.SYNTHETIC
+    returnType = "V"
+    strings = listOf("basicIntegrity", "ctsProfileMatch", "MEQ")
+    customFingerprint = { methodDef, _ ->
+        methodDef.parameters.size == 1 && 
+        methodDef.parameterTypes.first() == "Lcom/google/android/gms/tasks/Task;"
+    }
 }
 
 internal val spoofSignatureFingerprint = fingerprint {
     custom { methodDef, classDef ->
-        methodDef.name == "getCertificateSHA256Fingerprint" && classDef.type.endsWith("Lorg/telegram/messenger/AndroidUtilities;")
+        methodDef.name == "getCertificateSHA256Fingerprint" && 
+        (classDef.type.endsWith("Lorg/telegram/messenger/AndroidUtilities;") || 
+         classDef.type.endsWith("Luz/unnarsx/cherrygram/AndroidUtilities;")) // Added Cherrygram path
     }
 }
