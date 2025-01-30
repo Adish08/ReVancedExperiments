fff01f400de58c45d2b7d5702aa44aa2282ebc36
Adish Sagarawat <69076256+Adish08@users.noreply.github.com>
2025-01-30 13:54:12 +0530

    Add Cherrygram Path

--- patches/src/main/kotlin/li/auna/patches/telegram/downloadboost/Fingerprints.kt
+++ patches/src/main/kotlin/li/auna/patches/telegram/downloadboost/Fingerprints.kt
@@ -1,10 +1,12 @@
-package li.auna.patches.telegram.downloadboost
-
-import app.revanced.patcher.fingerprint
-
 internal val updateParamsFingerprint = fingerprint {
     returns("V")
+    strings("preloadPrefixSize", "downloadChunkSizeBig")
     custom { methodDef, classDef ->
-        methodDef.name == "updateParams" && classDef.type.endsWith("Lorg/telegram/messenger/FileLoadOperation;")
+        methodDef.name == "updateParams" && 
+        classDef.type.let {
+            it.endsWith("FileLoadOperation;") && 
+            (it.contains("org/telegram") || it.contains("cherrygram")) 
+        } &&
+        methodDef.parameters.isEmpty()
     }
 }
