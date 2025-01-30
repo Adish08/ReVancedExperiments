b34e39d7061f738e56a3a93108e325949a1e839e
Adish Sagarawat <69076256+Adish08@users.noreply.github.com>
2025-01-30 13:27:31 +0530

    Add Cherrygram Path

--- patches/src/main/kotlin/li/auna/patches/telegram/disableautoupdate/Fingerprints.kt
+++ patches/src/main/kotlin/li/auna/patches/telegram/disableautoupdate/Fingerprints.kt
@@ -6,7 +6,9 @@ import com.android.tools.smali.dexlib2.Opcode
 internal val checkAppUpdateFingerprint = fingerprint {
     returns("V")
     custom { methodDef, classDef ->
-        methodDef.name == "checkAppUpdate" && classDef.type.endsWith("Lorg/telegram/ui/LaunchActivity;")
+        methodDef.name == "checkAppUpdate" && 
+        (classDef.type.endsWith("Lorg/telegram/ui/LaunchActivity;") ||
+         classDef.type.endsWith("Luz/unnarsx/cherrygram/ui/LaunchActivity;"))
     }
     opcodes(
         Opcode.IF_NEZ,
@@ -19,13 +21,17 @@ internal val checkAppUpdateFingerprint = fingerprint {
 internal val setNewAppVersionAvailableFingerprint = fingerprint {
     returns("Z")
     custom { methodDef, classDef ->
-        methodDef.name == "setNewAppVersionAvailable" && classDef.type.endsWith("Lorg/telegram/messenger/SharedConfig;")
+        methodDef.name == "setNewAppVersionAvailable" && 
+        (classDef.type.endsWith("Lorg/telegram/messenger/SharedConfig;") ||
+         classDef.type.endsWith("Luz/unnarsx/cherrygram/messenger/SharedConfig;")) 
     }
 }
 
 internal val blockViewUpdateFingerprint = fingerprint {
     returns("V")
     custom { methodDef, classDef ->
-        methodDef.name == "show" && classDef.type.endsWith("Lorg/telegram/ui/Components/BlockingUpdateView;")
+        methodDef.name == "show" && 
+        (classDef.type.endsWith("Lorg/telegram/ui/Components/BlockingUpdateView;") ||
+         classDef.type.endsWith("Luz/unnarsx/cherrygram/ui/Components/BlockingUpdateView;"))
     }
 }
